031600178be115cabe1ea5753a1991b085916b2e
Sean Luchen <spamseanluchen@gmail.com>
2018-07-27 00:27:12 -0700

    finished up projects + added stars

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_27_044322) do
+ActiveRecord::Schema.define(version: 2018_07_27_065044) do
 
   create_table "projects", force: :cascade do |t|
-    t.string "email"
-    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "owner"
+    t.string "collaborators"
+    t.string "stakeholders"
+    t.string "description"
+    t.string "tasks"
+    t.string "milestones"
+    t.string "stars"
+    t.string "team"
   end
 
   create_table "users", force: :cascade do |t|
