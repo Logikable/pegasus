f7500d0f3ba985284193a715676be5f4e27341e9
Charlie Zhang <czhang@wish.com>
2018-07-27 01:05:26 -0700

    Style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user
+  helper_method :current_user, :current_page
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def current_page
+    @current_page = request.original_fullpath
   end
 end
