e4dd79a44e86aca9f680b26fce6f233166a5de60
Charlie Zhang <czhang@wish.com>
2018-07-26 22:36:27 -0700

    Style

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
+    if current_user.nil?
+      redirect_to :action => "login"
+    else 
+      render "index"
+    end
+  end
+
+  def login
+    render "login"
   end
 end
