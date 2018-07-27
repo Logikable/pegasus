4f21fc67f4366cd16e069faa905e000c1a643841
Charlie Zhang <czhang@wish.com>
2018-07-26 20:53:04 -0700

    Add google auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'home/index'
+
+  resources :sessions, only: [:create, :destroy]
+  resource :home, only: [:index]
+  resources :user
+
+  root 'home#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
